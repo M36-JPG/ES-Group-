b4fd519f5c5223513b42668909aac9ff35162388
sma08608-beep <sma08608@gmail.com>
2026-04-15 01:06:40 +0200

    Update 7_Segment with keypad.c

--- Keypad/7_Segment with keypad.c
+++ Keypad/7_Segment with keypad.c
@@ -25,12 +25,3 @@ if(copy_U8Number <= 9){
     GPIO_VidSetPortDirection (copy_U8Port, SevenSeg_Arr[copy_U8Number]);
 }
 }
-
-// u8 number[10]={0x3F , 0x06 , 0x5B , 0x4F , 0x66 , 0x6D , 0x7D , 0x07 , 0x7F , 0x6F};
-// void Seven_Segment(u8 copy_u8port){
-//     GPIO_VidSetPortDirection(copy_u8port , GPIO_OUTPUT_ALL);
-
-// }
-// void Seven_Segment_Display(u8 number , u8 copy_u8port){
-//     GPIO_VidSetPortValue(copy_u8port , numbers[number]);
-// }
