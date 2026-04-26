5cc2f9957626477b911ebd616dcf9f4bdf9acbde
Marwan Mohamed <maro.340ahmed@gmail.com>
2026-04-27 01:15:10 +0300

    Modify KEYPAD interface for 3x4 key layout

--- Final_Project/KEYPAD_interface.c
+++ Final_Project/KEYPAD_interface.c
@@ -2,14 +2,25 @@
 #include "GPIO_interface.h"
 #include "KEYPAD_interface.h"
 
-u8 Keys[4][4] = {{'1','2','3','A'},{'4','5','6','B'},{'7','8','9','C'},{'*','0','#','D'}};
-u8 Rows[4] = {PIN0, PIN1, PIN2, PIN3};
-u8 Cols[4] = {PIN4, PIN5, PIN6, PIN7};
+
+u8 Keys[4][3] = {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'},
+    {'*','0','#'}
+};
+
+u8 Rows[4] = {PIN0, PIN1, PIN2, PIN3}; 
+u8 Cols[3] = {PIN4, PIN5, PIN6};       
 
 void KEYPAD_Init(void) {
+   
     for(u8 i=0; i<4; i++) {
         GPIO_SetPIN_Direction(GPIO_PORTB, Rows[i], OUTPUT);
         GPIO_SetPIN_Value(GPIO_PORTB, Rows[i], HIGH);
+    }
+  
+    for(u8 i=0; i<3; i++) {
         GPIO_SetPIN_Direction(GPIO_PORTB, Cols[i], INPUT);
         GPIO_SetPIN_Value(GPIO_PORTB, Cols[i], HIGH); 
     }
@@ -17,15 +28,15 @@ void KEYPAD_Init(void) {
 
 u8 KEYPAD_GetKey(void) {
     for(u8 r=0; r<4; r++) {
-        GPIO_SetPIN_Value(GPIO_PORTB, Rows[r], LOW);
-        for(u8 c=0; c<4; c++) {
+        GPIO_SetPIN_Value(GPIO_PORTB, Rows[r], LOW); 
+        for(u8 c=0; c<3; c++) {
             if(GPIO_GetPIN_Value(GPIO_PORTB, Cols[c]) == LOW) {
-                while(GPIO_GetPIN_Value(GPIO_PORTB, Cols[c]) == LOW);
+                while(GPIO_GetPIN_Value(GPIO_PORTB, Cols[c]) == LOW); 
                 GPIO_SetPIN_Value(GPIO_PORTB, Rows[r], HIGH);
                 return Keys[r][c];
             }
         }
-        GPIO_SetPIN_Value(GPIO_PORTB, Rows[r], HIGH);
+        GPIO_SetPIN_Value(GPIO_PORTB, Rows[r], HIGH); 
     }
     return 0;
 }
